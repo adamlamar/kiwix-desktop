b19d96f5f15ad4719c9802e56b3f7984479003c4
Adam Lamar <adamlamar@users.noreply.github.com>
2025-12-08 21:57:49 -0700

    WIP

--- scripts/build-msix.ps1
+++ scripts/build-msix.ps1
@@ -79,7 +79,7 @@ try {
     # Required Qt libraries for kiwix-desktop
     $QtLibs = @(
         "Qt5Core.dll",
-        "Qt5Gui.dll", 
+        "Qt5Gui.dll",
         "Qt5Widgets.dll",
         "Qt5Network.dll",
         "Qt5WebEngine.dll",
@@ -93,7 +93,9 @@ try {
         "Qt5Qml.dll",
         "Qt5QmlModels.dll",
         "Qt5TextToSpeech.dll"
-    )    $copiedLibs = 0
+    )
+
+    $copiedLibs = 0
     if ($QtBinPath -and (Test-Path $QtBinPath)) {
         foreach ($lib in $QtLibs) {
             $libPath = Join-Path $QtBinPath $lib
